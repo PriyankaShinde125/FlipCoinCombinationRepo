c8caa0f81ef435bdf6ac84a2af3a46b9a5a80d29
“PriyankaShinde” <pkshinde125@gmail.com>
2022-10-16 18:23:19 +0530

    [Priyanka] added siglet combination and win percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,26 @@
 #! /bin/bash -x
 
+declare  -A singlet
 isHead=1
+headWon=0
+tailWon=0
+echo "enter number of times do you want to play"
+read n
+for ((i=0;i<n;i++))
+do
 toss=$((RANDOM%2))
 if [ $toss -eq $isHead ]
 then
-	echo "Head wins"
+	singlet['H']=$((headWon++))
 	else
-	echo "tail wins"
+	singlet['T']=$((tailWon++))
 fi
+done
+
+echo "head  $headWon times"
+echo "tail $tailWon times"
+headWonPer=$(( $headWon*100/$n ))
+tailWonPer=$(( $tailWon*100/$n ))
+
+echo "Head win percentage= $headWonPer"
+echo "Tail win percentage= $tailWonPer"
